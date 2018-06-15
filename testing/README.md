ae2dfdf4be28cc8b5718cbfa8f13d34c0b1b9d54
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-06-15 01:14:22 -0300

    requirements

---
+++ testing/README.md
@@ -0,0 +1,4 @@
+Para rodar os testes, digite:
+```python
+	python -m unittest discover tests
+```
--- testing/tests/test_nada.py
+++ testing/tests/test_nada.py
@@ -1,7 +1,8 @@
 import unittest
-from src import nada
-from src import nadi
+from src import nada  # Importa o arquivo nadda.py
+from src import nadi  # Importa a variavel nadi, que eh exportada pelo __init__.py de src/
 class TestNadaNadi(unittest.TestCase):
+	"""Exemplos de teste envolvendo importação de módulos próprios.."""
 	mnada = nada
 	mnadi = nadi
 	def test_nada(self):
