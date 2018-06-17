50eb9823c57ed1ac2162db05496ba7c18999a37b
Leonardo Maffei da Silva <leoitu22hotmail.com@gmail.com>
2018-06-16 22:01:17 -0300

    Update acessaBanco.py
    modificaçõs para python3

--- acessaBanco.py
+++ acessaBanco.py
@@ -2,11 +2,14 @@ import MySQLdb
 
 # Cria conexao com o banco. No caso, caso você possua uma instância do MySQL rodando
 # localmente, pode-se atribuir ao parâmetro host o valor "localhost"
-conn_db = _mysql.connect(host="172.17.0.2", port=3306, user="root", passwd="root")
+conn_db = MySQLdb.connect(host="172.17.0.2", port=3306, user="root", passwd="root")
 
 # Mandar como parâmetro uma string, que será executada como um query SQL
-conn_db.query("create database trabd")
-conn_db.query("use trabd;")
+try:
+  conn_db.query("create database trabd")
+except:
+  pass
+conn_db.select_db("trabd")
 
 # Pronto! Usar o db.query() é como se estivesse na CLI do mysql.
 # PS: db.query("show databases;") DÁ ERRO e dps disso só consegui acessa o banco 
