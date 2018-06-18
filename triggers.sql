8c0d3e44f6412886c7773d35c08b0456f329f96d
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-06-17 22:00:45 -0300

    coloquem coisas puramente locais na pasta tmp, que sera ignorada

--- triggers.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM Partido WHERE id IN (
-    SELECT * FROM (
-        SELECT Partido.id FROM Candidato, Partido WHERE Candidato.id = Partido.id GROUP BY Partido.id HAVING ( COUNT(Candidato.id) = 0 )
-    ) AS p
-)
