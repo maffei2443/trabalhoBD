6a62e6d3c0b49afad5c4feeca6cbb6c3580ab95b
Caiogoia <caiogoia390@gmail.com>
2018-06-17 21:36:23 -0300

    com update

--- CRUD.py
+++ CRUD.py
@@ -2,27 +2,6 @@
 # -*- coding: UTF-8 -*-
 import MySQLdb
 
-def GetTables(conn_db):
-    cursor = conn_db.cursor()
-    
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
-    tablesNames = []
-    for table in tables:
-        tablesNames.append(table[0])
-
-    return tablesNames
-
-def GetColumns(conn_db, table):
-    cursor = conn_db.cursor()
-        
-
-    cursor.execute("SHOW columns FROM " + table)
-    
-    columns = cursor.fetchall()
-
-    return columns
-
 def Delete(conn_db, tabela):
     # No campo "passwd" coloca a senha correspondente ao seu MySQL
     cursor = conn_db.cursor()
@@ -34,16 +13,44 @@ def Delete(conn_db, tabela):
 
     return dados
 
-def Insert(conn_db, table, valuesNames, values):
+
+#######################################################################################################
+
+
+def Insert(conn_db):
     cursor = conn_db.cursor()
 
-    print("INSERT INTO " + table +
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    for table in tables:
+        print(table[0])
+    
+    print("Digite o nome da tabela na qual deseja inserir dados: ")
+    tableName = input()
+    cursor.execute("SHOW columns FROM " + tableName)
+    
+    columns = cursor.fetchall()
+    
+    valuesNames = ""
+    values = ""
+    for column in columns:
+        print("Digite um valor do tipo " + column[1] + " para a coluna " + column[0] + ": ")
+        value = input()
+        if len(valuesNames):
+            valuesNames += ", "
+        if len(values):
+            values += ", "
+        
+        valuesNames += column[0]
+        values += value
+
+    cursor.execute("INSERT INTO " + tableName +
                     " (" + valuesNames + ") "+
                     "VALUES (" + values + ");")
 
-    cursor.execute("INSERT INTO " + table +
-                    " (" + valuesNames + ") "+
-                    "VALUES (" + values + ");")
+
+#######################################################################################################
+
 
 def Read(conn_db, coluna, tabela):
     cursor = conn_db.cursor()
@@ -54,6 +61,11 @@ def Read(conn_db, coluna, tabela):
     dados = cursor.fetchall()
 
     return dados
+
+
+
+#######################################################################################################
+
 
 def Update(conn_db):
     # No campo "passwd" coloca a senha correspondente ao seu MySQL
@@ -84,9 +96,6 @@ def Update(conn_db):
         print("Digite um novo valor para o campo " + field + ": ")
         values.append( input())
 
-    for v in values:
-        print(v + "\n")
-
     # Monta e executa a Query
     QueryString = "UPDATE " + tableName + " SET "
 
@@ -94,13 +103,19 @@ def Update(conn_db):
     for field in fieldsName:
         QueryString += field + " = " + values[i]
         i += 1
-        if len(values):
+        if i == len(values) - 1:
             QueryString += ", "
+
+    print(QueryString)
 
     cursor.execute(QueryString)
     dados = cursor.fetchall()
 
     return dados
+
+
+#######################################################################################################
+
 
 def CreateDb(conn_db):
     # Usar cursor para fazer queries sql
