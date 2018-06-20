01110011619a602a49784f4cd8365480603876b5
luisfbgs <arueluis@gmail.com>
2018-06-19 22:00:23 -0300

    Análise estática realizada no módulo user_interface utilizando pylint

--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
-from CRUD import dao
-import MySQLdb as sql
 import base64
+from CRUD import dao
 
-def GetImg(img):
-    with open(img, "rb") as f:
-        ret = base64.b64encode(f.read())
+def get_img(img):
+    with open(img, "rb") as file:
+        ret = base64.b64encode(file.read())
         return ret
 
 def clear():
@@ -19,184 +18,184 @@ def clear():
         except:
             print("Não foi possível limpar a tela.")
 
-def ShowAtb(dataObj, tableName, id):
-    data = dataObj.GetAllTab( tableName, id)
-    columns = dataObj.GetColumns( tableName)
+def show_atb(data_obj, table_name, key):
+    data = data_obj.GetAllTab(table_name, key)
+    columns = data_obj.GetColumns(table_name)
 
     print("\nNome do atributo -- Valor")
     print("-------------------------")
     for i in range(len(data[0])):
         print(str(columns[i][0]) + " -- " + str(data[0][i]))
 
-def ShowColumns(dataObj, tableName):
-    columns = dataObj.GetColumns( tableName)
-    print("Atributos de " +  tableName + ":")
+def show_columns(data_obj, table_name):
+    columns = data_obj.GetColumns(table_name)
+    print("Atributos de " +  table_name + ":")
     for column in columns:
         print(column[0])
 
-def ShowIds(dataObj, tableName):
-    ids = dataObj.GetIds( tableName)
+def show_ids(data_obj, table_name):
+    keys = data_obj.GetIds(table_name)
 
     print("id -- Nome")
     print("-----------")
-    for item in ids:
+    for item in keys:
         print(str(item[0]) + " -- " + item[1])
 
-def CheckIds( tableName, oid):
-    ids = dataObj.GetIds( tableName)    
+def check_ids(data_obj, table_name, key):
+    keys = data_obj.GetIds(table_name)
 
     exist = False
-    for item in ids:
-        if(oid == str(item[0])):
+    for item in keys:
+        if key == str(item[0]):
             exist = True
     return exist
 
-def CheckColumn(dataObj, tableName, name):
-    columns = dataObj.GetColumns( tableName)
-    
+def check_column(data_obj, table_name, name):
+    columns = data_obj.GetColumns(table_name)
+
     exist = False
     for column in columns:
-        if(column[0] == name):
+        if column[0] == name:
             exist = True
     return exist
 
-def UserDelete(dataObj):
+def user_delete(data_obj):
     try:
-        print("UserDelete")
-        tables = dataObj.GetTables()
+        print("user_delete")
+        tables = data_obj.GetTables()
         for table in tables:
             print("-- " + table)
 
-        tableName = input("\nDigite o nome do tipo de dado que deseja deletar: ")
+        table_name = input("\nDigite o nome do tipo de dado que deseja deletar: ")
 
-        if(tableName not in tables):
+        if table_name not in tables:
             input("Não é possível deletar um dado do tipo desejado, aperte ENTER para voltar ao menu")
             return
 
-        ShowIds(dataObj, tableName)
+        show_ids(data_obj, table_name)
 
-        oid = input("Digite o nome da chave do objeto que deseja deletar(chave de candidatura é o candidato, id da tabela para as demais tabelas): ")
+        key = input("Digite o nome da chave do objeto que deseja deletar(chave de candidatura é o candidato, id da tabela para as demais tabelas): ")
 
-        if(CheckIds( tableName, oid) == False):
+        if check_ids(data_obj, table_name, key) == False:
             input("O id que se deseja deletar nao existe, aperte ENTER para voltar ao menu")
             return
 
-        dataObj.Delete(tableName, oid)    
-       
-    except Exception as e:
-        print(e)
+        data_obj.Delete(table_name, key)
+
+    except Exception as exception:
+        print(exception)
         input("Digite algo para voltar ao menu")
 
-def UserCreate(dataObj):
+def user_create(data_obj):
     try:
-        print("########## UserCreate ##########")
-        tables = dataObj.GetTables()
+        print("########## user_create ##########")
+        tables = data_obj.GetTables()
         for table in tables:
             print("-- " + table)
 
-        tableName = input("\nDigite o nome do tipo de dado que deseja inserir: ")
+        table_name = input("\nDigite o nome do tipo de dado que deseja inserir: ")
 
-        if(tableName not in tables):
+        if table_name not in tables:
             input("Não é possível inserir um dado do tipo desejado, aperte ENTER para voltar ao menu")
             return
-        
-        columns = dataObj.GetColumns( tableName)
 
-        valuesNames = ""
+        columns = data_obj.GetColumns(table_name)
+
+        values_names = ""
         values = ""
         for column in columns:
-            if(column[5] == "auto_increment"):
+            if column[5] == "auto_increment":
                 continue
-            if(column[2] == "YES"):
+            if column[2] == "YES":
                 option = input("Deseja inserir " + column[0] + "?(Y/N)")
-                if(option != 'Y' and option != 'y'):
+                if option != 'Y' and option != 'y':
                     continue
 
             value = input("Digite um valor do tipo " + column[1] + " para " + column[0] + ": ")
 
-            if(column[1] == "longblob"):
-                value = "\"" + str(GetImg(value)) + "\""
+            if column[1] == "longblob":
+                value = "\"" + str(get_img(value)) + "\""
                 print("A")
-            
-            if len(valuesNames):
-                valuesNames += ", "
+
+            if len(values_names):
+                values_names += ", "
             if len(values):
                 values += ", "
-            
-            valuesNames += column[0]
+
+            values_names += column[0]
             values += value
 
-        dataObj.Insert( tableName, valuesNames, values)
+        data_obj.Insert(table_name, values_names, values)
 
-    except Exception as e:
-        print(e)
+    except Exception as exception:
+        print(exception)
         input("Digite algo para voltar ao menu")
 
-def UserUpdate(dataObj):
+def user_update(data_obj):
     try:
-        print("########## UserUpdate ##########")
-        tables = dataObj.GetTables()
+        print("########## user_update ##########")
+        tables = data_obj.GetTables()
         for table in tables:
             print("-- " + table)
 
-        tableName = input("\nDigite o nome do tipo de dado que deseja atualizar: ")
+        table_name = input("\nDigite o nome do tipo de dado que deseja atualizar: ")
 
-        if(tableName not in tables):
+        if table_name not in tables:
             input("Não é possível atualizar um dado do tipo desejado, aperte ENTER para voltar ao menu")
             return
-        
-        ShowIds(dataObj, tableName)
-        
-        id = input("\nDigite o id do item que deseja atualizar: ")
 
-        if(CheckIds( tableName, id) == False):
+        show_ids(data_obj, table_name)
+
+        key = input("\nDigite o id do item que deseja atualizar: ")
+
+        if not check_ids(data_obj, table_name, key):
             input("O id que se deseja atualizar nao existe, aperte ENTER para voltar ao menu")
             return
 
-        ShowAtb(dataObj, tableName, id)
-        
+        show_atb(data_obj, table_name, key)
+
         name, value = input("Digite o nome do valor e o novo valor que deseja atribuir separados por espaco: ").split(" ")
 
-        if(CheckColumn( tableName, name) == False):
+        if not check_column(data_obj, table_name, name):
             input("O atributo que se deseja atualizar nao existe, aperte ENTER para voltar ao menu")
             return
 
 
-        if(name == "foto"):
-            value = "\"" + str(GetImg(value)) + "\""
+        if name == "foto":
+            value = "\"" + str(get_img(value)) + "\""
             print("A")
 
-        dataObj.Update( tableName, name, value, id)
+        data_obj.Update(table_name, name, value, key)
         input("Aperte ENTER para retornar ao menu")
-    except Exception as e:
-        print(e)
+    except Exception as exception:
+        print(exception)
         input("Digite algo para voltar ao menu")
 
-def UserRead(dataObj):
+def user_read(data_obj):
     try:
-        print("########## UserRead ##########")
+        print("########## user_read ##########")
 
-        tables = dataObj.GetTables()
+        tables = data_obj.GetTables()
         for table in tables:
             print("-- " + table)
 
-        tableName = input("\nDigite o nome do tipo de dado que deseja ler: ")
+        table_name = input("\nDigite o nome do tipo de dado que deseja ler: ")
 
-        if(tableName not in tables):
+        if table_name not in tables:
             input("Não é possível ler um dado do tipo desejado, aperte ENTER para voltar ao menu")
             return
 
-        ShowColumns( tableName)
+        show_columns(data_obj, table_name)
 
         atbs = input("Digite os nomes dos atributos que deseja ver separados por virgula (sem espaços): ")
         columns = atbs.split(",")
 
         for column in columns:
-            if(CheckColumn( tableName, column) == False):
+            if not check_column(data_obj, table_name, column):
                 input("Não é possível ler o dado " + column + " aperte ENTER para voltar ao menu")
                 return
 
-        data = dataObj.Read( atbs, tableName)
+        data = data_obj.Read(atbs, table_name)
 
         for item in data:
             print("\nAtributo -- Valor")
@@ -205,15 +204,15 @@ def UserRead(dataObj):
                 print(str(columns[i]) + ": " + str(item[i]))
 
         input("\nDigite ENTER para retornar ao menu")
-    except Exception as e:
-        print(e)
+    except Exception as exception:
+        print(exception)
         input("Digite algo para voltar ao menu")
 
-def UserSpecial(dataObj):
+def user_special(data_obj):
     try:
         # cursor = conn_db.cursor()
 
-        print("########## UserSpecial ##########")
+        print("########## user_special ##########")
         print("#1 - Candidatos de um local                           #")
         print("#2 - Candidatos de um partido                         #")
         print("#3 - Partidos de uma coligacao                        #")
@@ -221,52 +220,50 @@ def UserSpecial(dataObj):
 
         option = input("# Opção: ")
 
-        if(option == "1" or option == "Candidatos de um local"):
+        if option == "1" or option == "Candidatos de um local":
             clear()
             local = input("Digite o nome do local: ")
-            data = dataObj.CandidatoGetLocal( "Local", local)
+            data = data_obj.CandidatoGetLocal("Local", local)
             for item in data:
                 print("Id -- Nome")
                 print("----------------------")
                 print(str(item[0]) + "--" + str(item[1]) + "\n")
             input("Digite ENTER para voltar ao menu")
 
-        elif(option == "2" or option == "Candidatos de um partido"):
+        elif option == "2" or option == "Candidatos de um partido":
             clear()
             partido = input("Digite o nome do partido: ")
-            data = dataObj.CandidatoGetPartido( "Partido", partido)
+            data = data_obj.CandidatoGetPartido("Partido", partido)
             for item in data:
                 print("Id -- Nome")
                 print("----------------------")
                 print(str(item[0]) + "--" + str(item[1]) + "\n")
             input("Digite ENTER para voltar ao menu")
-        
-        elif(option == "3" or option == "Partidos de uma coligacao"):
+
+        elif option == "3" or option == "Partidos de uma coligacao":
             clear()
             colig = input("Digite o nome da coligacao: ")
-            data = dataObj.PartidoGetColig( "Coligacao", colig)
+            data = data_obj.PartidoGetColig("Coligacao", colig)
             for item in data:
                 print("Id -- Nome")
                 print("----------------------")
                 print(str(item[0]) + "--" + str(item[1]) + "\n")
             input("Digite ENTER para voltar ao menu")
-       
-        # elif(option == "4" or option == "Candidatos de um local e partido especificos"):
+
+        # elif option == "4" or option == "Candidatos de um local e partido especificos":
         #     clear()
         #     colig = input("Digite o nome do local: ")
-        #     data = dataObj.PartidoGetColig( "Coligacao", colig)
+        #     data = data_obj.PartidoGetColig("Coligacao", colig)
         #     for item in data:
         #         print("Id -- Nome")
         #         print("----------------------")
         #         print(str(item[0]) + "--" + str(item[1]))
         #     input("Digite ENTER para voltar ao menu")
-    except Exception as e:
-        print(e)
+    except Exception as exception:
+        print(exception)
         input("Digite algo para voltar ao menu")
 
-if __name__ == "__main__":
-
-
+def main():
     user = input("Digite o nome do usuario mysql: ")
     passwd = input("Digite a senha do usuario mysql: ")
 
@@ -275,21 +272,21 @@ if __name__ == "__main__":
 
     # Cria conexao com o banco. No caso, caso voce possua uma instancia do MySQL rodando
     # localmente, pode-se atribuir ao parametro host o valor "localhost"
-    dataObj = dao()
-    dataObj.conn_db(host="localhost", port=3306, user=user, passwd=passwd)
-    op = input("Deseja criar/resetar o banco? (Y/N) ")
+    data_obj = dao()
+    data_obj.conn_db(host="localhost", port=3306, user=user, passwd=passwd)
+    option = input("Deseja criar/resetar o banco? (Y/N) ")
 
-    if(op == "Y" or op == "y"):
-        dataObj.CreateDb()
-    dataObj.conn_db(host="localhost", db="mydb", port=3306, user=user, passwd=passwd)
+    if option == "Y" or option == "y":
+        data_obj.CreateDb()
+    data_obj.conn_db(host="localhost", db="mydb", port=3306, user=user, passwd=passwd)
 
-    STAY = True
+    stay = True
 
-    while(STAY):
+    while stay:
         clear()
         print("###################")
         print("# Inserir             - 1   #")
-        print("# Atualizar           - 2   #") 
+        print("# Atualizar           - 2   #")
         print("# Ler                 - 3   #")
         print("# Remover             - 4   #")
         print("# Consulta especial   - 5   #")
@@ -298,23 +295,26 @@ if __name__ == "__main__":
         print("###################")
         option = input("# Opção: ")
 
-        if(option == "1" or option == "Inserir"):
+        if option == "1" or option == "Inserir":
             clear()
-            UserCreate(dataObj)
-        elif(option == "2" or option == "Atualizar"):
+            user_create(data_obj)
+        elif option == "2" or option == "Atualizar":
             clear()
-            UserUpdate(dataObj)
-        elif(option == "3" or option == "Ler"):
+            user_update(data_obj)
+        elif option == "3" or option == "Ler":
             clear()
-            UserRead(dataObj)
-        elif(option == "4" or option == "Remover"):
+            user_read(data_obj)
+        elif option == "4" or option == "Remover":
             clear()
-            UserDelete(dataObj)
-        elif(option == "5" or option == "Consulta especial"):
+            user_delete(data_obj)
+        elif option == "5" or option == "Consulta especial":
             clear()
-            UserSpecial(dataObj)
-        elif(option == "6" or option == "Sair"):
+            user_special(data_obj)
+        elif option == "6" or option == "Sair":
             clear()
-            STAY = False 
-    
-    dataObj.close()
+            stay = False
+
+    data_obj.close()
+
+if __name__ == "__main__":
+    main()
